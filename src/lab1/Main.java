6d6960d6a160000819b3f58b8532cecfbe0346f3
Choro-bit <elchoro.aitbaev@mail.utoronto.ca>
2025-09-11 17:18:10 -0400

    fixed a syntax problem

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -3,7 +3,7 @@ package lab1;
 public class Main {
     public static void main(String[] args) {
         FizzBuzz.goFizzBuzz(5);
-        int i = 0
+        int i = 0;
         while (i<=13) {
             FizzBuzz.goFizzBuzz(13);
         }
