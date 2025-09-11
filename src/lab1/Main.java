543153abb76b0f6a3394c8054c79d14d0ea8188a
Choro-bit <elchoro2004@gmail.com>
2025-09-11 17:15:16 -0400

    Update Main.java
    added a while loop

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -3,5 +3,9 @@ package lab1;
 public class Main {
     public static void main(String[] args) {
         FizzBuzz.goFizzBuzz(5);
+        int i = 0
+        while (i<=13) {
+            FizzBuzz.goFizzBuzz(13);
+        }
     }
 }
