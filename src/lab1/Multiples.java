b86392f1ff0c573e9a53785bf7f682ee26355869
Choro-bit <elchoro.aitbaev@mail.utoronto.ca>
2025-09-12 22:03:54 -0400

    Created Multiples.java

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,18 +1,28 @@
 package lab1;
 
 public class Multiples {
+
     public static void main(String[] args) {
-        int a = 1 ;
-        int DivisibleNumbers = 0 ;
-        while (a<1000) {
-            boolean multipleOf3 = a % 3 == 0;
-            boolean multipleOf5 = a % 5 == 0;
-            if (multipleOf3 || multipleOf5) {
-                DivisibleNumbers++;
-            }
-            a++;
-        }
-        System.out.println(DivisibleNumbers);
+        int count = multiples(1000, 3, 5);  // same as before, but now flexible
+        System.out.println(count);
     }
 
+    // Generalized multiples method
+    public static int multiples(int n, int a, int b) {
+        int number = 1;
+        int divisibleNumbers = 0;
+
+        while (number < n) {
+            boolean multipleOfA = number % a == 0;
+            boolean multipleOfB = number % b == 0;
+
+            if (multipleOfA || multipleOfB) {
+                divisibleNumbers++;
+            }
+
+            number++;
+        }
+
+        return divisibleNumbers;
+    }
 }
